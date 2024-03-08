55559c018fc4b3a853c44a0b8d92b28a01eaf578
Maxence <maxcha.cv@gmail.com>
2024-03-09 00:30:48 +0100

    add disposable

---
+++ src/main/kotlin/Disposable.kt
@@ -0,0 +1,20 @@
+
+class CompositeDisposable {
+
+    private val disposables: MutableList<Disposable> = mutableListOf()
+
+    operator fun plusAssign(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun add(vararg disposable: Disposable) {
+        disposables.addAll(disposable)
+    }
+
+    fun dispose() {
+        disposables.forEach(Disposable::invoke)
+        disposables.clear()
+    }
+}
+
+typealias Disposable = () -> Unit
