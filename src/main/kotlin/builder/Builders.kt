27b47b0399b231de1b72ed440ab2138a3f589661
Maxence <maxcha.cv@gmail.com>
2024-03-09 01:08:27 +0100

    add builder for basic type

--- src/main/kotlin/builder/Builders.kt
+++ src/main/kotlin/builder/Builders.kt
@@ -3,3 +3,10 @@ package builder
 import type.MutableReactiveType
 
 fun int(value: Int): MutableReactiveType<Int> = MutableReactiveType(value)
+fun long(value: Long): MutableReactiveType<Long> = MutableReactiveType(value)
+fun float(value: Float): MutableReactiveType<Float> = MutableReactiveType(value)
+fun double(value: Double): MutableReactiveType<Double> = MutableReactiveType(value)
+fun string(value: String): MutableReactiveType<String> = MutableReactiveType(value)
+fun short(value: Short): MutableReactiveType<Short> = MutableReactiveType(value)
+fun char(value: Char): MutableReactiveType<Char> = MutableReactiveType(value)
+fun boolean(value: Boolean): MutableReactiveType<Boolean> = MutableReactiveType(value)
